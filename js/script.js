183cde2f710a6368c9c98b3e507ce6bc997912af
Jacob Yoon <jacobyoon926@gmail.com>
2019-12-31 12:13:59 -0500

    Init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    } else {
+    }
+}
